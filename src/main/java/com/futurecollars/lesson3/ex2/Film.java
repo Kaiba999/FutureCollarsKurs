c754c2f1904891a5c2ac1c91338ba8e6f7c37ee2
Damian Rossa <damianrossa1993@o2.pl>
2022-09-30 18:26:11 +0200

    Dodano zad 2 z lekcji 3.

---
+++ src/main/java/com/futurecollars/lesson3/ex2/Film.java
@@ -0,0 +1,15 @@
+package com.futurecollars.lesson3.ex2;
+
+public class Film {
+    private String title;
+    private String nameAndSurnameCreator;
+    private int timeOfFilm;
+    private int productionYear;
+
+    public Film(String title, String nameAndSurnameCreator, int timeOfFilm, int productionYear) {
+        this.title = title;
+        this.nameAndSurnameCreator = nameAndSurnameCreator;
+        this.timeOfFilm = timeOfFilm;
+        this.productionYear = productionYear;
+    }
+}
