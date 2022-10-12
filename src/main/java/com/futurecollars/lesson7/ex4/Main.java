5e9622adbf96112155962001633382ae9eaf3a9d
Damian Rossa <damianrossa1993@o2.pl>
2022-10-12 19:16:20 +0200

    Dodano zad 7 z lekcji 4.

---
+++ src/main/java/com/futurecollars/lesson7/ex4/Main.java
@@ -0,0 +1,8 @@
+package com.futurecollars.lesson7.ex4;
+
+public class Main {
+    public static void main(String[] args) {
+        TextService textService = new TextService();
+        System.out.println(textService.geTextLength("cxzc"));
+    }
+}
